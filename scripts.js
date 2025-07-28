ee306e5df6552a793f876586689f4265a9bff322
ryansantosfernandes <ryan.sfernandes20009@gmail.com>
2025-07-28 09:30:40 -0300

    desafio DevClub finalizado

--- scripts.js
+++ scripts.js
@@ -1,22 +1,24 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
+const currencySelected = document.querySelector(".currency-selected")
 
-
-function convertValues(){
+function convertValues() {
    const inputCurrencyValue = +document.querySelector("#input-value").value
    const currencyValueConvert = document.querySelector(".currency-value-to-convert")
    const currencyValueConverted = document.querySelector(".currency-value")
 
    const dolarToday = 5.2
    const euroToday = 6.2
+   const libraToday = 7.2
+   const bitcoinToday = 660.202
    const convertedValue = inputCurrencyValue / dolarToday
 
 
    if (currencySelect.value === "dolar") {
       currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-       style: "currency",
-       currency: "USD"
-   }).format(convertedValue)
+         style: "currency",
+         currency: "USD"
+      }).format(convertedValue)
    }
 
    if (currencySelect.value === "euro") {
@@ -24,13 +26,28 @@ function convertValues(){
          style: "currency",
          currency: "EUR"
       }).format(inputCurrencyValue / euroToday)
-      
+
    }
 
 
+   if (currencySelect.value === "libra") {
+      currencyValueConverted.innerHTML = new Intl.NumberFormat('en-GB', {
+         style: 'currency',
+         currency: 'GBP',
+      }).format(inputCurrencyValue / libraToday)
+   }
+
+
+   if (currencySelect.value === "bitcoin") {
+      currencyValueConverted.innerHTML = new Intl.NumberFormat('en-US', {
+         style: 'currency',
+         currency: 'BTC'
+      }).format(inputCurrencyValue / bitcoinToday)
+   }
+
    currencyValueConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-       style: "currency",
-       currency: "BRL"
+      style: "currency",
+      currency: "BRL"
    }).format(inputCurrencyValue)
 
    console.log(currencySelect.value)
@@ -39,23 +56,33 @@ function convertValues(){
 
 
 
-function changeCurrency(){
-   const currencyName = document.getElementById("dolar")
-   const currencyImage = document.querySelector("#dolar-americano")
+   function changeCurrency() {
+      const currencyName = document.getElementById("dolar")
+      const currencyImage = document.querySelector("#dolar-americano")
 
-   if (currencySelect.value === "dolar") {
-      currencyName.innerHTML = "Dólar americano"
-      currencyImage.src = "./assets/estados-unidos (1) 1.png"
+      if (currencySelect.value === "dolar") {
+         currencyName.innerHTML = "Dólar americano"
+         currencyImage.src = "./assets/estados-unidos (1) 1.png"
+      }
+      if (currencySelect.value === "euro") {
+         currencyName.innerHTML = "Euro"
+         currencyImage.src = "./assets/euro.png"
+      }
+
+      if (currencySelect.value ==="libra") {
+         currencyName.innerHTML = "Libra Esterlina"
+         currencyImage.src = "./assets/libra.png"
+      }
+
+      if (currencySelect.value === "bitcoin") {
+         currencyName.innerHTML = "Bitcoin"
+         currencyImage.src = "./assets/bitcoin.png"
+      }
+
+      convertValues()
    }
-   if (currencySelect.value === "euro") {
-      currencyName.innerHTML = "Euro"
-      currencyImage.src = "./assets/euro.png"
-   }
 
-   convertValues()
-}
-
-currencySelect.addEventListener("change", changeCurrency)
-convertButton.addEventListener("click", convertValues)
+   currencySelect.addEventListener("change", changeCurrency)
+   convertButton.addEventListener("click", convertValues)
 
 
